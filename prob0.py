abb811a0c01aca10eb1ac0117bcba11b9e1e0671
priyansh gupta <37746818+priyanshgupta1998@users.noreply.github.com>
2018-11-25 09:36:19 +0530

    Update prob0.py

--- prob0.py
+++ prob0.py
@@ -2,4 +2,11 @@ import hashlib
 import sha3
 sk = sha3.shake_128() 
 sk.update(b'')
-print([sk.squeeze(512)])
+print([sk.squeeze(512)])  
+
+'''when i'm trying to run this code (in python 3) then it's throwing an error 
+>>> print([sk.squeeze(512)])
+AttributeError: '_pysha3.shake_128' object has no attribute 'squeeze'
+
+'''
+
